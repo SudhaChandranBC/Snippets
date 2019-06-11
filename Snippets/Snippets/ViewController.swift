062ec96fcb79a3f5998000c39c1d3b104e7e1a3f
Chandran, Sudha <sudhachandran1bc@gmail.com>
2019-06-11 05:44:56 +0530

    DispatchGroup and DispatchSemaphore

--- Snippets/Snippets/ViewController.swift
+++ Snippets/Snippets/ViewController.swift
@@ -9,12 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var sharedResource = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        dispatchGroupSnippet()
+        dispatchSemaphoreSnippet()
     }
 
+    func fetchImage(completion: @escaping (UIImage?, Error?) -> ()) {
+        guard let url = URL(string: "https://www.opencollege.info/wp-content/uploads/2016/02/relaxation-skills.jpg") else { return }
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            completion(UIImage(data: data ?? Data()), nil)
+        }.resume()
+    }
 
+    func dispatchGroupSnippet() {
+        let dispatchGroup = DispatchGroup()
+        
+        dispatchGroup.enter()
+        fetchImage { (_, _) in
+            print("dispatchGroup:Finished fetching image 1")
+            dispatchGroup.leave()
+        }
+        
+        dispatchGroup.enter()
+        fetchImage { (_, _) in
+            print("dispatchGroup:Finished fetching image 2")
+            dispatchGroup.leave()
+        }
+        
+        dispatchGroup.enter()
+        fetchImage { (_, _) in
+            print("dispatchGroup:Finished fetching image 3")
+            dispatchGroup.leave()
+        }
+        
+        dispatchGroup.notify(queue: .main) {
+            print("finished fetching images with DispatchGroup")
+        }
+        
+        print("waitng to fetch images...")
+    }
+    
+    func dispatchSemaphoreSnippet() {
+        let semaphore = DispatchSemaphore(value: 0)
+
+        let dispatchQueue = DispatchQueue.global(qos: .background)
+        
+        dispatchQueue.async {
+            
+            self.fetchImage { (_, _) in
+                print("DispatchSemaphore: Finished fetching image 1")
+                self.sharedResource.append("1")
+                semaphore.signal()
+            }
+            semaphore.wait()
+
+            self.fetchImage { (_, _) in
+                print("DispatchSemaphore: Finished fetching image 2")
+                self.sharedResource.append("2")
+                semaphore.signal()
+            }
+            semaphore.wait()
+            
+            self.fetchImage { (_, _) in
+                print("DispatchSemaphore: Finished fetching image 3")
+                self.sharedResource.append("3")
+                semaphore.signal()
+            }
+            semaphore.wait()
+        }
+        print("DispatchSemaphore: Start fetching images")
+        
+    }
 }
 
